c5402468bfff6dd3d6db8969a53586592049b687
Brize Glace <viktorovitchromain@gmail.com>
2023-04-02 12:49:31 +0200

    patch color bug on navbar & footer

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 var countDownDate = new Date("Oct 18, 2023 20:00:00").getTime();
 
 // Mettre à jour le décompte toutes les secondes
-var x = setInterval(function() {
+var x = setInterval(function () {
 
   // Obtenir la date et l'heure actuelles
   var now = new Date().getTime();
-  
+
   // Calculer la différence entre la date de fin et la date actuelle
   var distance = countDownDate - now;
-  
+
   // Calculer les jours, heures, minutes et secondes restantes
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -33,29 +33,45 @@ function getRandomColor() {
   var letters = '0123456789ABCDEF';
   var color = '#';
   for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+    color += letters[Math.floor(Math.random() * 16)];
   }
 
   // calculer la luminosité de la couleur
-  var r = parseInt(color.substring(1,3),16);
-  var g = parseInt(color.substring(3,5),16);
-  var b = parseInt(color.substring(5,7),16);
+  var r = parseInt(color.substring(1, 3), 16);
+  var g = parseInt(color.substring(3, 5), 16);
+  var b = parseInt(color.substring(5, 7), 16);
   var luminosity = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
 
   return {
-      color: color,
-      luminosity: luminosity
+    color: color,
+    luminosity: luminosity
   };
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var randomColor = getRandomColor();
+  var navbarColor = document.querySelectorAll("ul li a")
+  var footerColor = document.querySelectorAll("p")
   document.body.style.backgroundColor = randomColor.color;
   document.body.style.color = randomColor.luminosity < 0.5 ? "white" : "black";
+  navbarColor.forEach(function(item) {
+    item.style.color = randomColor.luminosity < 0.5 ? "white" : "black";
+  });
+  footerColor.forEach(function(item) {
+    item.style.color = randomColor.luminosity < 0.5 ? "white" : "black";
+  });
 });
 
-window.addEventListener("beforeunload", function() {
+window.addEventListener("beforeunload", function () {
   var randomColor = getRandomColor();
+  var navbarColor = document.querySelectorAll("ul li a")
+  var footerColor = document.querySelectorAll("p a")
   document.body.style.backgroundColor = randomColor.color;
   document.body.style.color = randomColor.luminosity < 0.5 ? "white" : "black";
+  navbarColor.forEach(function(item) {
+    item.style.color = randomColor.luminosity < 0.5 ? "white" : "black";
+  });
+  footerColor.forEach(function(item) {
+    item.style.color = randomColor.luminosity < 0.5 ? "white" : "black";
+  });
 });
